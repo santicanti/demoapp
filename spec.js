1b326411228adb46321e0a3315f2a1619003129e
Santiago <santiago.canti.gcba@gmail.com>
2017-05-20 11:22:33 -0300

    added a test that fails

--- spec.js
+++ spec.js
@@ -10,7 +10,7 @@ describe('loading express', function () {
   it('responds to /', function testSlash(done) {
   request(server)
     .get('/')
-    .expect(200, done);
+    .expect(500, done);
   });
   it('404 everything else', function testPath(done) {
     request(server)
